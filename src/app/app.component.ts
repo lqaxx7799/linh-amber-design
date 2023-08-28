f1fe2fa27427d6cd8ec4e834ccb01c10462e24d6
lqaxx7799 <lqaxx7799@gmail.com>
2023-08-28 22:58:10 +0700

    add experience card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'linh-amber-design';
   public isMenuOpen = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(filter(value => value instanceof NavigationEnd))
+      .subscribe({
+        next: () => {
+          if (this.isMenuOpen) {
+            this.isMenuOpen = false;
+            document.body.style.overflow = 'auto';
+          }
+        }
+      });
+  }
+
   public toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    document.body.style.overflow = this.isMenuOpen ? 'hidden' : 'auto'
   }
 }
--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,12 +1,13 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {ExperienceCardComponent} from "./components/experience-card/experience-card.component";
 
 @Component({
   selector: 'app-experience',
   standalone: true,
   templateUrl: 'experience.component.html',
   styleUrls: ['experience.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule, ExperienceCardComponent]
 })
 export class ExperienceComponent {
 
