364ce0cd3dca286826d42821df3433c609aef8ee
lqaxx7799 <lqaxx7799@gmail.com>
2023-09-04 19:06:13 +0700

    photo

--- src/app/pages/experience/components/experience-photo/experience-photo.component.ts
+++ src/app/pages/experience/components/experience-photo/experience-photo.component.ts
@@ -1,12 +1,13 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {ImageViewerComponent} from "../../../../components/image-viewer/image-viewer.component";
 
 @Component({
   selector: 'app-experience-photo',
   standalone: true,
   templateUrl: 'experience-photo.component.html',
   styleUrls: ['experience-photo.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule, ImageViewerComponent]
 })
 export class ExperiencePhotoComponent {
 
