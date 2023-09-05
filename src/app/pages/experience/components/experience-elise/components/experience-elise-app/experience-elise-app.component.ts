a1c137ecd49d1dc3e120966774760c03171161a9
lqaxx7799 <lqaxx7799@gmail.com>
2023-09-06 00:59:58 +0700

    elise app, elise cover

--- src/app/pages/experience/components/experience-elise/components/experience-elise-app/experience-elise-app.component.ts
+++ src/app/pages/experience/components/experience-elise/components/experience-elise-app/experience-elise-app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {ImageViewerComponent} from "../../../../../../components/image-viewer/image-viewer.component";
 
 @Component({
   selector: 'app-experience-elise-app',
   templateUrl: 'experience-elise-app.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageViewerComponent],
 })
 export class ExperienceEliseAppComponent {}
--- src/app/pages/experience/components/experience-elise/components/experience-elise-cover/experience-elise-cover.component.ts
+++ src/app/pages/experience/components/experience-elise/components/experience-elise-cover/experience-elise-cover.component.ts
@@ -1,10 +1,11 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {ImageViewerComponent} from "../../../../../../components/image-viewer/image-viewer.component";
 
 @Component({
   selector: 'app-experience-elise-cover',
   templateUrl: 'experience-elise-cover.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageViewerComponent],
 })
 export class ExperienceEliseCoverComponent {}
