8de942653aadaa70ba5e09f4bd9a1df95239186c
anhlq1 <anhlq1@novaon.asia>
2023-09-06 18:43:39 +0700

    elise psot

--- src/app/pages/experience/components/experience-elise/components/experience-elise-post/experience-elise-post.component.ts
+++ src/app/pages/experience/components/experience-elise/components/experience-elise-post/experience-elise-post.component.ts
@@ -1,10 +1,11 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {ImageViewerComponent} from "../../../../../../components/image-viewer/image-viewer.component";
 
 @Component({
   selector: 'app-experience-elise-post',
   templateUrl: 'experience-elise-post.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageViewerComponent],
 })
 export class ExperienceElisePostComponent {}
