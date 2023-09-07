4aac8e0aa683e16f5fbda503e4e08f5287068cef
anhlq1 <anhlq1@novaon.asia>
2023-09-07 13:10:55 +0700

    elise com

--- src/app/pages/experience/components/experience-elise/components/experience-elise-ecom/experience-elise-ecom.component.ts
+++ src/app/pages/experience/components/experience-elise/components/experience-elise-ecom/experience-elise-ecom.component.ts
@@ -1,10 +1,11 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {ImageViewerComponent} from "../../../../../../components/image-viewer/image-viewer.component";
 
 @Component({
   selector: 'app-experience-elise-ecom',
   templateUrl: 'experience-elise-ecom.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageViewerComponent],
 })
 export class ExperienceEliseEcomComponent {}
